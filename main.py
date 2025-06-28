b58aaa87ac05f8dc9749ec17d631ede189a5e34e
notice96 <danil_pm@top-xteam.com>
2025-06-28 17:51:47 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 import os
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Приложение работает!"}
+    return {"message": "Hello from Railway!"}
 
 if __name__ == "__main__":
+    import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
