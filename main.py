7e6ab549d9e30591bb8a21bce8d26a2590c2b188
notice96 <danil_pm@top-xteam.com>
2025-07-02 21:13:40 +0500

    main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ KEITARO_BASE_URL = os.getenv("KEITARO_BASE_URL")
 AFFILIATE_NETWORKS = {
     "ExGaming": 54,
     "Glory Partners": 14,
-    "4RA PARTNER": 17
+    "4RA PARTNER": 17,
+    "TSL": 55
 }
 
 OFFER_GROUPS = {
